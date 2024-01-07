a4c5ada16b3c440a7f0c9e692b531f3a3cb7f871
priyanshupatel1044@gmail.com <priyanshupatel1044@gmail.com>
2024-01-08 01:15:51 +0530

    Add initial project files

---
+++ server/index.js
@@ -0,0 +1,92 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://priyanshu:priyanshu1234@cluster0.svwgo43.mongodb.net/thala_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB connection established successfully');
+});
+
+// Define a schema for the stored data
+const thalaSchema = new mongoose.Schema({
+    input: { type: String, required: true },
+    result: String,
+});
+
+const ThalaModel = mongoose.model('ThalaModel', thalaSchema);
+
+// Add the following code inside the /check route handler
+
+// Add the following code inside the /check route handler
+
+app.post('/check', async (req, res) => {
+    const input = req.body.input;
+  
+    // Convert input to uppercase
+    const inputUpperCase = input.toUpperCase();
+  
+    // Check if the input is a number or can be converted to a number
+    const isNumber = !isNaN(parseFloat(inputUpperCase)) && isFinite(inputUpperCase);
+  
+    // Check if the total number of characters is 7
+    const has7Characters = inputUpperCase.toString().length === 7;
+  
+    // Calculate the sum of digits
+    const sum = isNumber ? inputUpperCase.toString().split('').reduce((acc, digit) => acc + parseInt(digit), 0) : 0;
+  
+    // Determine the final result based on the conditions
+    let resultMessage = '';
+    let resultStoredMessage = '';
+    if (sum === 7) {
+      const sequence = inputUpperCase.toString().split('').join('+');
+      resultMessage = `${sequence}=${sum} 💛 Thala For A Reason 💛`;
+      resultStoredMessage = `${sequence}=${sum}`;
+    } else if (has7Characters) {
+        const sequence = inputUpperCase.toString().split('').join('+');
+      resultMessage = `${sequence}= 7 💛 Thala For A Reason 💛`;
+      resultStoredMessage = `${sequence}= 7`;
+    } else {
+      resultMessage = 'Input does not meet the criteria';
+    }
+  
+    // Check if the input (in uppercase) already exists in the database
+    const existingData = await ThalaModel.findOne({ input: inputUpperCase });
+  
+    // Save to MongoDB only if the conditions are met and the data doesn't already exist
+    if ((sum === 7 || has7Characters) && !existingData) {
+      const thalaData = new ThalaModel({
+        input: inputUpperCase,
+        result: resultStoredMessage,
+      });
+      await thalaData.save();
+    }
+  
+    // Send the response
+    res.json({ message: resultMessage });
+  });
+  
+app.get('/getThalaData', async (req, res) => {
+  try {
+    // Fetch all saved data from MongoDB
+    const thalaData = await ThalaModel.find({});
+    res.json({ thalaData });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DarkModeTwoToneIcon from '@mui/icons-material/DarkModeTwoTone';
+// import './App.css';
+
+function App() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+  const [thalaData, setThalaData] = useState([]);
+  const [theme, setTheme] = useState('dark'); // 'light' or 'dark'
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/check', { input });
+      setResult(response.data.message);
+
+      // Fetch updated data immediately after submitting
+      fetchThalaData();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    // Check if the pressed key is Enter (key code 13)
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+
+  const fetchThalaData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/getThalaData');
+      setThalaData(response.data.thalaData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchThalaData();
+  }, []); // Fetch data on component mount
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <div className={`flex items-center justify-center h-screen ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
+      <div className={`p-8 rounded shadow-md w-full max-w-lg ${theme === 'dark' ? 'bg-gray-800' : 'bg-white'}`}>
+        <div className='fixed top-2 right-2'>
+          <button
+            className={`p-2 rounded-full bg-${theme === 'light' ? 'white' : 'black'} text-${theme === 'light' ? 'black' : 'white'} hover:bg-${theme === 'light' ? 'gray-200' : 'gray-800'} focus:outline-none`}
+            onClick={toggleTheme}
+          >
+            {theme === 'light' ? <DarkModeTwoToneIcon style={{ color: 'black' }} /> : <DarkModeTwoToneIcon style={{ color: 'white' }} />}
+          </button>
+        </div>
+        <h1 className={`text-4xl font-bold text-center mb-8 ${theme === 'dark' ? 'text-yellow-300' : 'text-gray-900'}`}>Thala for a Reason</h1>
+        <div className="flex items-center space-x-4 mb-4">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyPress={handleKeyPress}
+            placeholder="Enter a string or number"
+            className={`px-4 py-2 border rounded w-full focus:outline-none ${theme === 'dark' ? 'border-gray-700 text-black' : 'border-gray-300 text-black'}`}
+          />
+          <button
+            className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none ml-4 ${theme === 'dark' ? 'bg-blue-800' : ''}`}
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+        {result && (
+          <p className={`text-lg ${result.includes('Input does not meet the criteria') ? 'text-red-500' : 'text-green-500'}`}>
+            {result}
+          </p>
+        )}
+        <div className="mt-8">
+          <h2 className={`text-2xl font-bold mb-4 ${theme === 'dark' ? 'text-yellow-300' : 'text-gray-900'}`}>💛 Thala For A Reason 💛</h2>
+          <div className="table-container max-h-96 overflow-y-auto">
+            <table className={`min-w-full border ${theme === 'dark' ? 'border-gray-700' : 'border-gray-300'}`}>
+              <thead>
+                <tr>
+                  <th className={`py-2 px-4 border-b ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Input</th>
+                  <th className={`py-2 px-4 border-b ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Result</th>
+                </tr>
+              </thead>
+              <tbody>
+                {thalaData.map((data, index) => (
+                  <tr key={index} className={index % 2 === 0 ? `${theme === 'dark' ? 'bg-gray-700' : 'bg-gray-100'}` : ''}>
+                    <td className={`py-2 px-4 border-b ${theme === 'dark' ? 'text-white' : 'text-black'}`}>{data.input}</td>
+                    <td className={`py-2 px-4 border-b ${theme === 'dark' ? 'text-white' : 'text-black'}`}>{data.result}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
