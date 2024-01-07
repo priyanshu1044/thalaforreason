cc7f3117bec87fc7b1f51880024499a8591a2706
priyanshupatel1044@gmail.com <priyanshupatel1044@gmail.com>
2024-01-08 02:01:56 +0530

    Add dotenv package for environment variables

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+
+dotenv.config({
+  path: "../.env"
+});
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://priyanshu:priyanshu1234@cluster0.svwgo43.mongodb.net/thala_db', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
